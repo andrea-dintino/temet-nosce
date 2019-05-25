35cccbd4fc4c38b49cecdf00c01d978cee9a0071
Puria Nafisi Azizi <puria@dyne.org>
2019-05-25 17:12:50 +0200

    :card_file_box: Adding useful info into the block page

--- src/templates/block.js
+++ src/templates/block.js
@@ -4,10 +4,56 @@ import {graphql} from  "gatsby"
 import Layout from "../components/layout"
 
 export default ({ data }) => {
-    console.log(data)
+    const {header, batches, header_signature} = data.sawtoothBlock
     return (
         <Layout>
-            ciao
+            <strong>{header_signature}</strong>
+            <br/> <br/>
+            <dl>
+                <dt>previous_block_id</dt>
+                <dd>{header.previous_block_id}</dd>
+
+                <dt>batch_ids</dt>
+                <dd>{header.batch_ids}</dd>
+
+                <dt>block_num</dt>
+                <dd>{header.block_num}</dd>
+
+                <dt>consensus</dt>
+                <dd>{header.consensus}}</dd>
+
+                <dt>state_root_hash</dt>
+                <dd>{header.state_root_hash}</dd>
+
+                {batches.map(b => {
+                    return (
+                        <div className="batch">
+                        {b.transactions.map(t => {
+                            return (
+                                <div className="transaction">
+                                    {t.header.family_name}/{t.header.family_version}
+                                    <dl>
+                                        <dt>inputs</dt>
+                                        {t.header.inputs.map(i=> {
+                                            return (<dd>{i}</dd>)
+                                        })}
+        
+                                        <dt>outputs</dt>
+                                        {t.header.outputs.map(o=> {
+                                            return (<dd>{o}</dd>)
+                                        })}
+                                    </dl>
+        
+                                    <h4>PAYLOAD</h4>
+                                    {t.payload}
+                                </div>
+                            )
+                        })}
+                        </div>
+                    )
+                })}
+
+            </dl>
         </Layout>
     )
 }
@@ -15,8 +61,24 @@ export default ({ data }) => {
 export const query = graphql`
     query($block_id:String!) {
         sawtoothBlock(header_signature: {eq: $block_id}) {
+            header_signature
             header {
-                block_num
+              previous_block_id
+              batch_ids
+              block_num
+              consensus
+              state_root_hash
+            }
+            batches {
+              transactions {
+                payload
+                header {
+                  family_name
+                  family_version
+                  inputs
+                  outputs
+                }
+              }
             }
         }
     }
